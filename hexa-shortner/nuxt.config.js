542bb10a409265adde93703edc12d742d0bd7080
Rubens Rafael <70234898+RubensRafael@users.noreply.github.com>
2022-05-09 15:17:25 -0300

    change icon

--- hexa-shortner/nuxt.config.js
+++ hexa-shortner/nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.ico' },
       { href:"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css", rel:"stylesheet", integrity:"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC", crossorigin:"anonymous"}
     ]
   },
